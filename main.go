0f24a36cc219bd76237da10bb0713e29d862c61f
NatsuiroGinga <duyueyeweiliang@gmail.com>
2023-05-10 10:26:45 +0800

    server-start

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
-package main
+package zinx
 
-import "log"
+import (
+	"time"
+	"zinx/lib/logger"
+)
 
-func main() {
-	log.Println("gg")
+func init() {
+	// 初始化日志
+	logger.Setup(&logger.Settings{
+		Path:       "logs",        // 日志文件路径
+		Name:       "zinx",        // 日志文件名称
+		Ext:        "log",         // 日志文件后缀
+		TimeFormat: time.DateOnly, // 时间格式
+	})
 }
--- znet/server.go
+++ znet/server.go
@@ -1,7 +1,10 @@
 package znet
 
 import (
-	"log"
+	"bufio"
+	"fmt"
+	"net"
+	"zinx/lib/logger"
 	"zinx/ziface"
 )
 
@@ -30,13 +33,62 @@ func NewServer(name string) ziface.IServer {
 }
 
 func (server *Server) Start() {
-	log.Printf("[Zinx] Server Name: %s starting...", server.Name)
+	go logger.Info(fmt.Sprintf("Server Listener at IP: %s, Port: %d, is starting...", server.IP, server.Port))
+	// 1. 创建socket
+	addr, err := net.ResolveTCPAddr(server.IPVersion, fmt.Sprintf("%s:%d", server.IP, server.Port))
+	if err != nil {
+		logger.Error(fmt.Sprintf("Resolve TCP Address failed: %s", err.Error()))
+		return
+	}
+	// 2. 监听服务器地址
+	listener, err := net.ListenTCP(server.IPVersion, addr)
+	if err != nil {
+		logger.Error(fmt.Sprintf("Listen TCP Address failed: %s", err.Error()))
+		return
+	}
+	go logger.Info(fmt.Sprintf("Server Listener at IP: %s, Port: %d, is started", server.IP, server.Port))
+	// 3. 阻塞等待客户端连接，处理客户端连接业务
+	for {
+		// 3.1 如果有客户端连接，阻塞返回
+		conn, err := listener.AcceptTCP()
+		if err != nil {
+			logger.Error(fmt.Sprintf("Accept TCP failed: %s", err.Error()))
+			continue
+		}
+		// 3.2 处理客户端业务
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	buf := make([]byte, 512)
+	var (
+		cnt int
+		err error
+	)
+
+	for {
+		cnt, err = reader.Read(buf)
+		if err != nil {
+			logger.Error(fmt.Sprintf("Read TCP failed: %s", err.Error()))
+			continue
+		}
+
+		logger.Info(fmt.Sprintf("Receive from client, cnt: %d, buf: %s", cnt, buf))
+
+		if _, err = conn.Write(buf[:cnt]); err != nil {
+			logger.Error(fmt.Sprintf("Write TCP failed: %s", err.Error()))
+			continue
+		}
+	}
 }
 
 func (server *Server) Stop() {
-	log.Printf("[Zinx] Server Name: %s stoping...", server.Name)
+	logger.Info("Server is stopping...")
 }
 
 func (server *Server) Serve() {
-	log.Printf("[Zinx] Server Name: %s serving...", server.Name)
+	logger.Info("Server is serving...")
+	server.Start() // 启动server服务
 }
