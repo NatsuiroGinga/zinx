143bf4773a66fc001b03331f3138cc873570a844
NatsuiroGinga <duyueyeweiliang@gmail.com>
2023-05-10 11:02:00 +0800

    util-string

--- lib/util/string.go
+++ lib/util/string.go
@@ -4,10 +4,12 @@ import (
 	"unsafe"
 )
 
+// String2Bytes converts string to []byte without copy.
 func String2Bytes(s string) (b []byte) {
 	return unsafe.Slice(unsafe.StringData(s), len(s))
 }
 
+// Bytes2String converts []byte to string without copy.
 func Bytes2String(b []byte) (s string) {
 	return *(*string)(unsafe.Pointer(&b))
 }
