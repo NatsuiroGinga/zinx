56c560d5111e9eb2a358c032f18d8ab4dbba305d
NatsuiroGinga <duyueyeweiliang@gmail.com>
2023-05-11 22:05:30 +0800

    handler

--- znet/connection.go
+++ znet/connection.go
@@ -15,11 +15,12 @@ type Connection struct {
 	isClosed   bool               // 当前的链接状态
 	exitChan   chan struct{}      // 告知当前链接已经退出的/停止的channel
 	msgHandler ziface.IMsgHandler // 消息管理模块
+	msgChan    chan []byte        // 无缓冲管道，用于读、写两个goroutine之间的消息通信
 }
 
 // StartReader 链接的读业务方法
 func (conn *Connection) startReader() {
-	logger.Info("Reader Goroutine is running...")
+	logger.Info("[Reader Goroutine is running...]")
 	defer func() {
 		logger.Info("connId = ", conn.connId, " Reader is exit, remote addr is ", conn.RemoteAddr())
 		conn.Stop()
@@ -35,11 +36,13 @@ func (conn *Connection) startReader() {
 		// 读取客户端的数据到buf中
 		if _, err = io.ReadFull(conn.conn, header); err != nil {
 			logger.Error("conn Read error: ", err)
+			conn.exitChan <- struct{}{}
 			break
 		}
 		// 拆包，得到msgId和dataLen放在msg中
 		if msg, err = dataPack.Unpack(header); err != nil {
 			logger.Error("unpack error: ", err)
+			conn.exitChan <- struct{}{}
 			break
 		}
 		// 根据dataLen再次读取data，放在msg.Data中
@@ -51,17 +54,35 @@ func (conn *Connection) startReader() {
 			}
 			msg.SetData(data)
 		}
-		// 调用当前链接所绑定的router的PreHandle方法等
-		request := NewRequest(conn, msg)
-
 		// 从路由中，找到注册绑定的Conn对应的router调用
 		go func() {
-			err = conn.msgHandler.Handle(request)
+			err = conn.msgHandler.Handle(NewRequest(conn, msg))
 			if err != nil {
 				logger.Error("connId = ", conn.connId, " handle error: ", err)
 				return
 			}
 		}()
+	}
+}
+
+// StartWriter 链接的写业务方法, 从chan中获取数据，然后写给客户端
+func (conn *Connection) startWriter() {
+	logger.Info("Writer Goroutine is running...")
+	defer func() {
+		logger.Info("connId = ", conn.connId, " Writer is exit, remote addr is ", conn.RemoteAddr())
+		conn.Stop()
+	}()
+
+	for {
+		select {
+		case data := <-conn.msgChan:
+			if _, err := conn.conn.Write(data); err != nil {
+				logger.Error("send data error: ", err)
+				return
+			}
+		case <-conn.exitChan:
+			return
+		}
 	}
 }
 
@@ -71,6 +92,7 @@ func (conn *Connection) Start() {
 	// 1. 启动从当前链接的读数据业务
 	go conn.startReader()
 	// 2. 启动从当前链接写数据业务
+	go conn.startWriter()
 }
 
 // Stop 停止链接，结束当前链接的工作
@@ -82,8 +104,11 @@ func (conn *Connection) Stop() {
 	conn.isClosed = true
 	// 1. 关闭socket链接
 	_ = conn.conn.Close()
-	// 2. 关闭该链接全部管道
+	// 2. 通知从缓冲队列读数据的业务，该链接已经关闭
+	conn.exitChan <- struct{}{}
+	// 3. 关闭该链接全部管道
 	close(conn.exitChan)
+	close(conn.msgChan)
 }
 
 func (conn *Connection) TcpConnection() *net.TCPConn {
@@ -101,6 +126,7 @@ func (conn *Connection) RemoteAddr() net.Addr {
 	panic("implement me")
 }
 
+// Send 发送数据，将数据发送给管道
 func (conn *Connection) Send(msgId uint32, data []byte) (err error) {
 	// 1. 判断当前链接是否已经关闭
 	if conn.isClosed {
@@ -113,11 +139,8 @@ func (conn *Connection) Send(msgId uint32, data []byte) (err error) {
 		logger.Error("pack error: ", err)
 		return
 	}
-	// 3. 写回客户端
-	if _, err = conn.conn.Write(packedBytes); err != nil {
-		logger.Error("write error: ", err)
-		return
-	}
+	// 3. 写到管道
+	conn.msgChan <- packedBytes
 
 	return
 }
@@ -130,5 +153,6 @@ func NewConnection(conn *net.TCPConn, connID uint32, msgHandler ziface.IMsgHandl
 		isClosed:   false,
 		exitChan:   make(chan struct{}, 1),
 		msgHandler: msgHandler,
+		msgChan:    make(chan []byte),
 	}
 }
